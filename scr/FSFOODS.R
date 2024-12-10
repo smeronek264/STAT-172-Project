c54c0bfba9c577c5f76656a9acbfb1d46403bf4f
smeronek264 <sophie.meronek@drake.edu>
2024-12-10 12:26:26 -0600

    Update FSFOODS.R

--- scr/FSFOODS.R
+++ scr/FSFOODS.R
@@ -14,20 +14,75 @@ library(tigris)
 source("code/clean_cps.R") # clean CPS
 source("code/clean_acs.R") # clean ACS
 
+# 
+cps_data = subset(cps_data, select = -c(FSTOTXPNC_perpers, FSSTATUSMD, FSSTATUS, FSWROUTY, FSBAL, FSRAWSCRA, FSTOTXPNC))
+cps_data = cps_data[complete.cases(cps_data), ]
+
 ##### EXPLORATORY ANALYSIS #####
 
 # Potential X Variables:
 # hhsize, female, hispanic, black, kids, elderly, education, married, faminc
 
-table(cps_data$Kitchen.Qual)
+cps_data = cps_data %>% 
+  mutate(FSFOODS_factor = as.factor(FSFOODS))
 
+ggplot(data=cps_data)+
+  geom_bar(aes(x=married, fill=FSFOODS_factor), position="fill") +
+  labs(x="Number of People who are Married", y="Proportion") +
+  ggtitle("Food Insecurity Over Marriage") +
+  scale_fill_grey("Food Variety\nInsecurity") +
+  theme_minimal()
 
-##### SPLITTING DATA #####
+ggplot(data=cps_data)+
+  geom_histogram(aes(x=female/hhsize, fill = FSFOODS_factor), binwidth = 0.1, position = "fill") +
+  labs(x="Percentage of Women in a Household", y="Proportion") +
+  ggtitle("Food Insecurity Over Women Proportion Size") +
+  scale_fill_grey("Food Variety\nInsecurity") +
+  theme_minimal()
 
-cps_data = subset(cps_data, select = -c(FSTOTXPNC_perpers, FSSTATUSMD, FSSTATUS, FSWROUTY, FSBAL, FSRAWSCRA, FSTOTXPNC))
+ggplot(data=cps_data)+
+  geom_histogram(aes(x=hispanic/hhsize, fill = FSFOODS_factor), binwidth = 0.1, position = "fill") +
+  labs(x="Percentage of Women in a Household", y="Proportion") +
+  ggtitle("Food Insecurity Over Women Proportion Size") +
+  scale_fill_grey("Food Variety\nInsecurity") +
+  theme_minimal()
 
-cps_data = cps_data[complete.cases(cps_data), ]
+ggplot(data=cps_data)+
+  geom_histogram(aes(x=black/hhsize, fill = FSFOODS_factor), binwidth = 0.1, position = "fill") +
+  labs(x="Percentage of Women in a Household", y="Proportion") +
+  ggtitle("Food Insecurity Over Women Proportion Size") +
+  scale_fill_grey("Food Variety\nInsecurity") +
+  theme_minimal()
 
+ggplot(data=cps_data)+
+  geom_histogram(aes(x=kids/hhsize, fill = FSFOODS_factor), binwidth = 0.1, position = "fill") +
+  labs(x="Percentage of Women in a Household", y="Proportion") +
+  ggtitle("Food Insecurity Over Women Proportion Size") +
+  scale_fill_grey("Food Variety\nInsecurity") +
+  theme_minimal()
+
+ggplot(data=cps_data)+
+  geom_histogram(aes(x=elderly/hhsize, fill = FSFOODS_factor), binwidth = 0.1, position = "fill") +
+  labs(x="Percentage of Women in a Household", y="Proportion") +
+  ggtitle("Food Insecurity Over Women Proportion Size") +
+  scale_fill_grey("Food Variety\nInsecurity") +
+  theme_minimal()
+
+ggplot(data=cps_data)+
+  geom_bar(aes(x=married, fill = FSFOODS_factor)) +
+  labs(x="Number of People with a Collage Degree", y="Proportion") +
+  ggtitle("Food Insecurity by Education Level") +
+  scale_fill_grey("Food Variety\nInsecurity") +
+  theme_minimal()
+
+ggplot(data=cps_data)+
+  geom_histogram(aes(x=married/hhsize, fill = FSFOODS_factor), binwidth = 0.1) +
+  labs(x="Percentage of Women in a Household", y="Proportion") +
+  ggtitle("Food Insecurity Over Women Proportion Size") +
+  scale_fill_grey("Food Variety\nInsecurity") +
+  theme_minimal()
+
+##### SPLITTING DATA ########## SPLIFSFOODS_factorTTING DATA #####
 
 # Setting the seed to make sure the code can be reproduced on other machines
 RNGkind(sample.kind = "default")
@@ -76,7 +131,7 @@ lr_ridge_cv = cv.glmnet(x.train, # train MATRIX - without y
                         y.train, # train y VECTOR = y column
                         family=binomial(link=logit),
                         weights = as.integer(train.df$weight),
-                        alpha = 0)                       
+                        alpha = 0)  
 
 # these models try a range of lambda values (differing penalty parameters)
 # and then use CV to resitmate out of sample error of each lambda
@@ -97,8 +152,8 @@ best_ridge_lambda = lr_ridge_cv$lambda.min
 lr_lasso_coeff = coef(lr_lasso_cv, s = "lambda.min") %>%  as.matrix()
 lr_ridge_coeff = coef(lr_ridge_cv, s = "lambda.min") %>%  as.matrix()
 
-lr_lasso_coeff
-lr_ridge_coeff
+(lr_lasso_coeff)
+(lr_ridge_coeff)
 
 # again. undestanding 
 
@@ -152,8 +207,8 @@ ridge_rocCurve = roc(response = as.factor(test.df.preds$FSFOODS),
                      predictor = test.df.preds$ridge_pred,
                      levels = c("0", "1"))
 
-
 plot(lasso_rocCurve, print.thres = TRUE, print.auc = TRUE)
+
 
 plot(ridge_rocCurve, print.thres = TRUE, print.auc = TRUE)
 
@@ -163,39 +218,56 @@ test.df$lasso_pred = as.factor(ifelse(test.df.preds$lasso_pred > lasso_pi_star, 
 ridge_pi_star = coords(ridge_rocCurve, "best", ret = "threshold")$threshold[1]
 test.df$ridge_pred = as.factor(ifelse(test.df.preds$ridge_pred > ridge_pi_star, "1", "0"))
 
-#make data frame of lasso ROC info
+
+# Calculate the ROC data as you did before
+lasso_rocCurve = roc(response = as.factor(test.df.preds$FSFOODS),
+                     predictor = test.df.preds$lasso_pred,
+                     levels = c("0", "1"))
+ridge_rocCurve = roc(response = as.factor(test.df.preds$FSFOODS),
+                     predictor = test.df.preds$ridge_pred,
+                     levels = c("0", "1"))
+
+# Create data frames for Lasso and Ridge ROC information
 lasso_data <- data.frame(
   Model = "Lasso",
   Specificity = lasso_rocCurve$specificities,
   Sensitivity = lasso_rocCurve$sensitivities,
-  AUC = lasso_rocCurve$auc %>% as.numeric
+  AUC = as.numeric(lasso_rocCurve$auc)
 )
-#make data frame of ridge ROC info
+
 ridge_data <- data.frame(
   Model = "Ridge",
   Specificity = ridge_rocCurve$specificities,
   Sensitivity = ridge_rocCurve$sensitivities,
-  AUC = ridge_rocCurve$auc%>% as.numeric
+  AUC = as.numeric(ridge_rocCurve$auc)
 )
 
+# Combine Lasso and Ridge data into a single data frame
+roc_data <- rbind(lasso_data, ridge_data)
 
-
-# Combine all the data frames
-roc_data <- rbind( lasso_data, ridge_data)
-
+# Add a column 'text_y' to specify the y position for text labels
+roc_data <- roc_data %>%
+  mutate(text_y = case_when(
+    Model == "Lasso" ~ 0.75,  # For Lasso model, position label at y = 0.75
+    Model == "Ridge" ~ 0.65   # For Ridge model, position label at y = 0.65
+  ))
 
 # Plot the data
+ggplot() +
+  geom_line(aes(x = 1 - Specificity, y = Sensitivity, color = Model), data = roc_data, size =1.5, alpha = 0.75) +
+  geom_text(data = roc_data %>% group_by(Model) %>% slice(1), 
+            aes(x = 0.75, y = text_y, color = Model,
+                label = paste0(Model, " AUC = ", round(AUC, 3)))) +
+  scale_colour_brewer(palette = "Set2") +
+  labs(title = "Lasso and Ridge Regression for Food Variety",x = "1 - Specificity", y = "Sensitivity", color = "Model") +
+  theme_minimal()
 
-#ggplot() +
-#  geom_line(aes(x = 1 - Specificity, y = Sensitivity, color = Model),data = roc_data) +
-#  geom_text(data = roc_data %>% group_by(Model) %>% slice(1), 
-#            aes(x = 0.75, y = c(0.75, 0.65, 0.55), colour = Model,
-#                label = paste0(Model, " AUC = ", round(AUC, 3)))) +
-#  scale_colour_brewer(palette = "Paired") +
-#  labs(x = "1 - Specificity", y = "Sensitivity", color = "Model") +
-#  theme_minimal()
+# IF we set pi* = 0.861, we can achieve a specificity of 0.857
+# and sensitivity of 0.905
 
-
+# That is, we'll predict a loff 85.7% of the time when Serena actually loses
+# We'll predict a win 90.5% of the time when Serena actually wins 
+# Area under the curve is 0.928
 
 
 ##### PREDICTING ACS #####
